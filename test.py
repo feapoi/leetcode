92809ed2b38c35ac1af52ec674253fb9dd45e9dd
zyh <369215201@qq.com>
2018-09-30 17:51:46 +0800

    2018 9 30

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-a = [1,2,3]
-print(a[1:2])
+def fun1(n):
+    res = []
+    start = []
+    def fun2(c = start, a = 0, b = 0):
+        print(a,b,c)
+        if a + b == 6:
+            res.append(c)
+            return
+        if a < 3:
+            fun2(c + '1', a + 1, b)
+        if b < a:
+            fun2(c + '2', a, b + 1)
+    fun2()
+    return res
+print(fun1(1))
 
