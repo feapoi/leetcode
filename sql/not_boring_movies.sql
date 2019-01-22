1f3036544cb283a61b633ff464010484e217d22d
zyh <369215201@qq.com>
2019-01-22 20:09:51 +0800

    2019 1 22

---
+++ sql/not_boring_movies.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS cinema (id INT, movie VARCHAR(255), description VARCHAR(255), rating FLOAT(2, 1));
+TRUNCATE TABLE cinema;
+INSERT INTO cinema (id, movie, description, rating) VALUES ('1', 'War', 'great 3D', '8.9');
+INSERT INTO cinema (id, movie, description, rating) VALUES ('2', 'Science', 'fiction', '8.5');
+INSERT INTO cinema (id, movie, description, rating) VALUES ('3', 'irish', 'boring', '6.2');
+INSERT INTO cinema (id, movie, description, rating) VALUES ('4', 'Ice song', 'Fantacy', '8.6');
+INSERT INTO cinema (id, movie, description, rating) VALUES ('5', 'House card', 'Interesting', '9.1');
+
+SELECT * FROM cinema WHERE id % 2 = 1 AND description != 'boring' ORDER BY rating  DESC;
